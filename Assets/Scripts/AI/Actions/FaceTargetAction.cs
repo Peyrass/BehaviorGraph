f710111ddff75e53374760f858deb47d14f4975f
Peyrass <peyrastruco@gmail.com>
2026-04-17 16:56:56 +0200

    chaser CASI CASI listo

--- Assets/Scripts/AI/Actions/FaceTargetAction.cs
+++ Assets/Scripts/AI/Actions/FaceTargetAction.cs
@@ -13,17 +13,15 @@ public partial class FaceTargetAction : Action
     [SerializeReference] public BlackboardVariable<float> TurnSpeed;
 
     protected override Status OnUpdate()
-    {
+    { 
         if (Target.Value == null) return Status.Failure;
 
         Vector3 direction = (Target.Value.transform.position - Self.Value.transform.position).normalized;
         direction.y = 0; // No queremos que el enemigo se incline hacia arriba/abajo
 
-        if (direction != Vector3.zero)
-        {
-            Quaternion targetRotation = Quaternion.LookRotation(direction);
-            Self.Value.transform.rotation = Quaternion.Slerp(Self.Value.transform.rotation, targetRotation, Time.deltaTime * TurnSpeed.Value);
-        }
+        if (direction == Vector3.zero) return Status.Running;
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        Self.Value.transform.rotation = Quaternion.Slerp(Self.Value.transform.rotation, targetRotation, Time.deltaTime * TurnSpeed.Value);
 
         return Status.Running;
     }
